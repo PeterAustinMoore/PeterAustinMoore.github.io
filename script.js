47a84da7511a7be08593efec87260339656ebb43
Peter Moore <peter.moore@socrata.com>
2015-12-02 14:04:11 -0500

    clean slate

--- script.js
+++
@@ -1,5 +0,0 @@
-$document.ready(function() {
-   $("#dataCatelog").click(function(){ $("#dataCatelogPage").toggle(); }); 
-    
-    
-});
--- up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-git add --all
-git commit -m "a"
-git push origin master
