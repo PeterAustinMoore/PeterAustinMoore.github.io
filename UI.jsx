4d6944d3cd331fff41c8285174a8aa0d333d30b1
Peter Moore <peter.moore@socrata.com>
2015-12-08 11:33:02 -0800

    new site

---
+++ UI.jsx
@@ -0,0 +1,40 @@
+var UI = React.createClass({
+  getInitialState: function()
+  {
+     return ({
+     		hostURL: '',
+     		datasetId: '',
+     		headerData:'',
+     		dependent:'',
+     		independent:'',
+          	jsonData: ''
+      })
+  },
+  render: function() {
+
+    return (
+      <div>
+        Hello
+        <Views />
+      </div>
+      )
+  },
+  onHostSubmit: function(data, URL, id){
+  	console.log(data);
+  	this.setState({
+  		headerData: data,
+  		hostURL: URL,
+  		datasetId: id
+  	});
+  },
+  onDataSubmit: function(data, dep, ind){
+  	this.setState({
+  		dependent: dep,
+  		independent: ind,
+  		jsonData: data
+  	});
+  }
+  
+});
+
+React.render(<UI/>, document.body);
---
+++ Views.jsx
@@ -0,0 +1,7 @@
+var Views = React.createClass({
+
+	render: function()
+	{
+		return(<div>Views</div>)
+	}
+});
